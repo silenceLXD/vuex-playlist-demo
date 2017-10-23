e79c43a20725559a0dba567d531dc36589103aa2
lixd <lixd@svocloud.com>
2017-10-23 10:53:42 +0800

    lixd
    chushihua

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne'
+import ProductListTwo from './components/ProductListTwo'
+export default {
+  name: 'app',
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+body{
+  font-family: Ubuntul;
+  color: #2c3e50;
+}
+</style>
